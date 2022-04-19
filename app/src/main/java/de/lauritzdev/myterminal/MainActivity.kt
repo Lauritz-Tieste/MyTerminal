125b5c3a08bf3e05f50b7e2e7966d06847503db8
Lauritz Tieste <lauritz6000000@gmail.com>
2022-04-19 18:40:09 +0200

    moved bluetooth to MyBluetooth.kt

--- app/src/main/java/de/lauritzdev/myterminal/MainActivity.kt
+++ app/src/main/java/de/lauritzdev/myterminal/MainActivity.kt
@@ -1,17 +1,46 @@
 package de.lauritzdev.myterminal
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_1.setOnClickListener {
-            ButtonManager.onBtnClick(this, et_1, recycler_view)
+            ButtonManager.onBtnClick(this, et_1, log_recycler_view)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.connect -> {
+                startActivity(Intent(this, Connect::class.java))
+                true
+            }
+            R.id.settings -> {
+                true
+            }
+            R.id.save -> {
+                true
+            }
+            R.id.open -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
---
+++ app/src/main/java/de/lauritzdev/myterminal/MyBluetooth.kt
@@ -0,0 +1,56 @@
+package de.lauritzdev.myterminal
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.RecyclerView
+
+object MyBluetooth {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun initBluetooth(
+        context: Context,
+        bluetoothManager: BluetoothManager,
+        recyclerView: RecyclerView
+    ) {
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(
+                context,
+                "Your device doesn't support bluetooth! You can't use this app!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        if (bluetoothAdapter?.isEnabled == false) {
+            Toast.makeText(context, "Please enable bluetooth!", Toast.LENGTH_LONG).show()
+        }
+
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address
+            ConnectManager.init(context, recyclerView)
+            ConnectManager.addElement(Device(deviceName, deviceHardwareAddress))
+        }
+
+
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        bluetoothAdapter?.bondedDevices
+
+    }
+}
