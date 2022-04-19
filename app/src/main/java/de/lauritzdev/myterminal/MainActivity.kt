63f57238433441ffaafa415f9e104feb61f38109
Lauritz Tieste <lauritz6000000@gmail.com>
2022-04-19 12:08:14 +0200

    created sample button clickListeners in MainActivity.kt

--- app/src/main/java/de/lauritzdev/myterminal/MainActivity.kt
+++ app/src/main/java/de/lauritzdev/myterminal/MainActivity.kt
@@ -2,10 +2,15 @@ package de.lauritzdev.myterminal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LayoutController.init(this)
+        btn_1.setOnClickListener { Btn.btnClick(this, et_1) }
+        btn_2.setOnClickListener { Btn.btnClick(this, et_2) }
     }
 }
