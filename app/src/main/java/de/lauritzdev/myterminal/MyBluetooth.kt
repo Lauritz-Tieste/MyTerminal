3d7cdc32cf36304a80dbd7b2208d6b8b3ec78ad6
Lauritz Tieste <lauritz6000000@gmail.com>
2022-04-24 16:00:09 +0200

    added bluetooth logic

--- app/src/main/java/de/lauritzdev/myterminal/MyBluetooth.kt
+++ app/src/main/java/de/lauritzdev/myterminal/MyBluetooth.kt
@@ -1,56 +1,94 @@
 package de.lauritzdev.myterminal
 
-import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Build
-import android.widget.Toast
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.IOException
 
+@SuppressLint("StaticFieldLeak")
+@RequiresApi(Build.VERSION_CODES.M)
 object MyBluetooth {
+    private var job: Job? = null
+    private lateinit var context: Context
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun initBluetooth(
-        context: Context,
-        bluetoothManager: BluetoothManager,
-        recyclerView: RecyclerView
-    ) {
-        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-        if (bluetoothAdapter == null) {
-            Toast.makeText(
-                context,
-                "Your device doesn't support bluetooth! You can't use this app!",
-                Toast.LENGTH_LONG
-            ).show()
+    fun init(c: Context) {
+        context = c
+        BluetoothConnect(context).connect()
+
+        if (MainActivity.bluetoothSocket != null) {
+            // val discoverRequest = OneTimeWorkRequest.Builder(DiscoverWorker::class.java).build()
+            // WorkManager.getInstance(context).enqueue(discoverRequest)
+
+            job = GlobalScope.launch { readBtData() }
+        } else {
+            Msg.e("Bluetooth socket is null")
         }
+    }
 
-        if (bluetoothAdapter?.isEnabled == false) {
-            Toast.makeText(context, "Please enable bluetooth!", Toast.LENGTH_LONG).show()
+    private fun readBtData() {
+        val buffer = ByteArray(1024)
+        val bluetoothSocketInputStream = MainActivity.bluetoothSocket!!.inputStream
+        var bytes: Int
+
+        (MainActivity.context as Activity).findViewById<TextView>(R.id.txt_rx)
+            .append("---------- STARTED SESSION WITH ${MainActivity.connectedDeviceName} ---------\n\n\n")
+
+        while (MainActivity.isConnected && buffer.isNotEmpty()) {
+            bytes = bluetoothSocketInputStream.read(buffer)
+            val rxTxt: TextView =
+                (MainActivity.context as Activity).findViewById(R.id.txt_rx)
+
+            val oldContent: String = rxTxt.text.toString()
+            val readMessage = String(buffer, 0, bytes)
+
+            if (readMessage.length > 39) {
+                rxTxt.text = "$oldContent${readMessage.substring(0, 40)}"
+            } else {
+                rxTxt.text = "$oldContent$readMessage"
+            }
+
+            // if(rx_txt.getLineCount() > 5){
+            //     val scroll_amount: Int = scroll_amount + rx_txt.getLineHeight();
+            // rx_txt.scrollTo(0, scroll_amount);
         }
+    }
 
-        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
-        pairedDevices?.forEach { device ->
-            val deviceName = device.name
-            val deviceHardwareAddress = device.address
-            ConnectManager.init(context, recyclerView)
-            ConnectManager.addElement(Device(deviceName, deviceHardwareAddress))
+    fun sendCommand(context: Context, input: String) {
+        if (MainActivity.bluetoothSocket != null) {
+            try {
+                MainActivity.bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch (e: IOException) {
+                Msg.show(context, e.toString())
+                e.printStackTrace()
+            }
         }
+    }
 
+    suspend fun disconnect(context: Context, txtStatus: TextView, statusBar: LinearLayout) {
+        if (MainActivity.bluetoothSocket != null) {
+            try {
+                job!!.cancel()
+                delay(100)
+                MainActivity.isConnected = false
+                delay(100)
+                MainActivity.bluetoothSocket!!.close()
+                MainActivity.bluetoothSocket = null
 
+                StatusBar.handle(context, txtStatus, statusBar)
 
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Msg.show(context, e.toString())
+            }
         }
-        bluetoothAdapter?.bondedDevices
-
     }
 }
+
